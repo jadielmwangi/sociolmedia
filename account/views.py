70488b7ec6477045e573344c38c48f9fd974a3af
jadielmwangi <jedielmwangi@gmail.com>
2020-07-05 17:01:21 +0300

    adds a message functionality when user updates profile a message appears

--- account/views.py
+++ account/views.py
@@ -5,7 +5,14 @@ from .forms import LoginForm
 from django.contrib.auth.decorators import login_required
 
 from django.core.mail import send_mail
-from .forms import LoginForm, UserRegistrationForm
+# from .forms import LoginForm, UserRegistrationForm
+
+from .forms import LoginForm, UserRegistrationForm, \
+                                    UserEditForm, ProfileEditForm
+from .models import Profile
+
+from django.contrib import messages
+
 # Create your views here.
 
 def user_login(request):
@@ -49,7 +56,7 @@ def register(request):
             # Save the User object
             new_user.save()
             # Create the user profile
-            # Profile.objects.create(user=new_user)
+            Profile.objects.create(user=new_user)
             return render(request,
                           'account/register_done.html',
                           {'new_user': new_user})
@@ -63,24 +70,29 @@ def register(request):
 
 
 
-# def register(request):
-#     if request.method == 'POST':
-#         user_form = UserRegistrationForm(request.POST)
-#         if user_form.is_valid():
-#         # Create a new user object but avoid saving it yet
-#             new_user = user_form.save(commit=False)
-#         # Set the chosen password
-#             new_user.set_password(
-#                 user_form.cleaned_data['password'])
-#             # Save the User object
-#             new_user.save()
-#             return render(request,
-#                             'account/register_done.html',
-#                             {'new_user': new_user})
 
-#         else:
-#             user_form = UserRegistrationForm()
-#         return render(request,
-#                         'account/register.html',
-#                         {'user_form': user_form})                    
-                            
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user,
+                                data=request.POST)
+        profile_form = ProfileEditForm(
+                instance=request.user.profile,
+                data=request.POST,
+                files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Profile updated '\
+                                            'successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
+
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(
+                            instance=request.user.profile)
+    return render(request,
+            'account/edit.html',
+            {'user_form': user_form,
+            'profile_form': profile_form})
--- sociolproject/settings.py
+++ sociolproject/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'account.apps.AccountConfig',
 
 ]
 
@@ -140,6 +139,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 
 
 
