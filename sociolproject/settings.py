d6a7e4f21ab6b9b7c7edc5655aac407899577c3b
jadielmwangi <jedielmwangi@gmail.com>
2020-07-07 12:31:05 +0300

    adds jquery to bookmark

--- sociolproject/settings.py
+++ sociolproject/settings.py
@@ -32,12 +32,14 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'account.apps.AccountConfig',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'images.apps.ImagesConfig',
 
 ]
 
--- sociolproject/urls.py
+++ sociolproject/urls.py
@@ -22,6 +22,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
 
+    path('images/', include('images.urls', namespace='images')),
+
 ]
 
 
